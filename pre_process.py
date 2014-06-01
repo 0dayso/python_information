e85edf7cb81054f995703e2c21bda2938b070cb8
Han Sun <HanSun@1230.me>
2014-06-01 04:48:20 -0700

    Build up the modules

--- pre_process.py
+++ pre_process.py
@@ -2,7 +2,11 @@ __author__ = 'yuerzx'
 
 import re
 import time
+import hashlib
+from pymongo import MongoClient
 
+#Get ready for the database
+rental_collection = MongoClient().EZYProperty.rental
 domain = "yeeyi.com/bbs/"
 
 def yeeyi_title_process(item):
@@ -32,6 +36,8 @@ def yeeyi_title_process(item):
         return "Rental Type"
     elif "性别" and "要求" in item:
         return "Roommate Prefer"
+    elif "QQ" and "号码" in item:
+        return "QQ"
     else:
         return "Unknown!" + item
 
@@ -45,14 +51,13 @@ yeeyi_dict={
     'Share':'Room',
     '客厅':'living Room',
     '其它':'Other',
-    '个人':'Personal',
-
+    '个人':'Personal'
 }
 
 #TODO Start to handle the time and clean all the time up.
 def yeeyi_content_process(content):
     # Actually this is a dictionary function to convert chinese to english
-    not_find = "Unknown!" + content
+    not_find = "Unknown! " + str(content)
     return yeeyi_dict.get(content,not_find)
 
 def cleanup_marks(page_content):
@@ -67,7 +72,6 @@ def cleanup_marks(page_content):
         font_count += 1
     print("Finish all the cleaning up, I have find " + str(span_count) + " hidden span and " + str(font_count) + " hidden font")
 
-
 def information_pickup(page_content):
     #this function is aiming at sort up all the information.
     item={}
@@ -77,14 +81,19 @@ def information_pickup(page_content):
             item_title = str(yeeyi_title_process(p.span.text))
             p.span.extract()
             item_content = re.sub('\\xa0', '', p.text)
-            if item_title == "Property Rent":
-                item_content = yeeyi_content_process(item_content)
+            if item_title == "Property Rooms":
+                item_content = get_rooms(item_content)
             elif item_title == "Phone": #vaild phone number
                 phone_vaild = re.search("\d{9,11}", item_content)
                 if phone_vaild:
                     item_content = phone_vaild.group(0)
                 else:
                     item_content = "Unknown!" + item_content
+            #skip content
+            elif item_title == "Details":
+                item_content = item_content
+            elif item_title == "Address":
+                item_content = item_content
             else:
                 item_content = yeeyi_content_process(item_content)
             item[item_title] = item_content
@@ -99,6 +108,50 @@ def information_pickup(page_content):
             item['org_imgs'] = imgs
     return item
 
+
+def get_rooms(item):
+    #aim to get all the rooms such as bed room, living room, car park and so on
+    list_rooms = list(item)
+    #bedroom
+    position = [i for i, x in enumerate(list_rooms) if x == "室"]
+    if position:
+        bedroom = int(list_rooms[(position[0]-1)])
+    else:
+        bedroom = 0
+    position = [i for i, x in enumerate(list_rooms) if x == "厅"]
+    if position:
+        living_room = int(list_rooms[(position[0]-1)])
+    else:
+        living_room = 0
+    position = [i for i, x in enumerate(list_rooms) if x == "卫"]
+    if position:
+        bath_room = int(list_rooms[(position[0]-1)])
+    else:
+        bath_room = 0
+    position = [i for i, x in enumerate(list_rooms) if x == "阳"]
+    if position:
+        balcony = int(list_rooms[(position[0]-1)])
+    else:
+        balcony = 0
+    return {"bedroom":bedroom,"living Room": living_room, "Bath Room": bath_room, "Balcony":balcony}
+
+def front_page_links(home_index):
+    links= {}
+    article_id_raw = hashlib.md5()
+    home_links = home_index.find("ul", class_="cgl cl")
+    for ls in home_links.find_all("a"):
+        article_id_raw.update(ls['href'].encode("utf-8"))
+        links[article_id_raw.hexdigest()] = ls['href']
+        search_result = rental_collection.find_one({"md5":article_id_raw.hexdigest()},{'_id':1})
+        if search_result:
+            del links[article_id_raw.hexdigest()]
+    return links
+
+def get_page_title(index):
+    #get the title for current page
+    title = (index.find('a', id="thread_subject")).text
+    return str(title)
+
 def get_publish_time(index):
     #Get the publishing data
     time_stamp_pre = index.find('div', class_="pti")
@@ -107,5 +160,5 @@ def get_publish_time(index):
         time_stamp = time.strptime(time_stamp_pre.group(0),"%Y-%m-%d %H:%M")
         time_pub = time.strftime("%d-%m-%Y %H:%M", time_stamp)
     else:
-        time_pub = "Unknown!" + time_stamp_pre
+        time_pub = "Unknown!" + (index.find('div', class_="pti")).em.text
     return time_pub
--- reantal_yeeyi.py
+++ reantal_yeeyi.py
@@ -2,19 +2,51 @@ __author__ = 'yuerzx'
 from bs4 import BeautifulSoup
 import requests
 import pre_process
-import re
-import time
+
+#Basic setting area
+#start page
+home_url = "http://www.yeeyi.com/bbs/house.php?mod=list&filter=all&city=30&sortid=1&page="
 
 #hide the system as a chrome
 header = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.52 Safari/536.5"}
-html = requests.get("http://www.yeeyi.com/bbs/forum.php?mod=viewthread&tid=1481166", headers=header)
-index = BeautifulSoup(html.content,from_encoding='GBK')
-#Get the publishing data
-time_pub = pre_process.get_publish_time(index)
+#Get the first few pages of yeeyi
+for x in range(1, 2):
+    home_url = home_url + str(x)
+    home_html = requests.get(home_url, headers = header)
+    home_index = BeautifulSoup(home_html.content, from_encoding="gbk")
+    available_links = pre_process.front_page_links(home_index)
+rental = {}
+counter = 0
+for md5, link in available_links.items():
+    counter += 1
+    rental_id = pre_process.rental_collection.insert({"md5": md5})
+    rental[rental_id] = {}
+    rental[rental_id]["url"] = link
+    html = requests.get(link, headers=header)
+    index = BeautifulSoup(html.content,from_encoding='GBK')
+    #Get the publishing data
+    rental[rental_id]["publish_time"] = pre_process.get_publish_time(index)
+    #Get the title
+    rental[rental_id]["title"] = pre_process.get_page_title(index)
+    #Prepare all the documents content
+    page_content = index.find('td', class_='t_f')
+    pre_process.cleanup_marks(page_content)
+    items = pre_process.information_pickup(page_content)
+    for k, v in items.items():
+        rental[rental_id][k] = v
+    if counter == 5:
+        for ids, keys in rental.items():
+            result = pre_process.rental_collection.update({'_id': ids}, {"$set":keys}, upsert=False)
+            if result:
+                print("入库成功: " + str(rental[ids]['title']))
+        counter = 0
+        rental = {} #clean up the dict to save memory
 
-#Prepare all the documents content
-page_content = index.find('td', class_='t_f')
-pre_process.cleanup_marks(page_content)
-item = pre_process.information_pickup(page_content)
-item["publish_time"] = time_pub
-print(item)
+while rental:
+    for ids, keys in rental.items():
+            result = pre_process.rental_collection.update({'_id': ids}, {"$set":keys}, upsert=False)
+            if result:
+                print("入库成功: " + str(rental[ids]['title']))
+    rental = {}
+
+print(rental)
