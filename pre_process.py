3e6fd7c80c323c28cdbf0633fbe93e9e278c8ad9
Han Sun <HanSun@1230.me>
2014-06-01 01:29:35 -0700

    Build up the modules

--- pre_process.py
+++ pre_process.py
@@ -1,5 +1,10 @@
 __author__ = 'yuerzx'
 
+import re
+import time
+
+domain = "yeeyi.com/bbs/"
+
 def yeeyi_title_process(item):
     """
     :rtype : string
@@ -28,7 +33,7 @@ def yeeyi_title_process(item):
     elif "性别" and "要求" in item:
         return "Roommate Prefer"
     else:
-        return "Unknown Item!" + item
+        return "Unknown!" + item
 
 yeeyi_dict={
     '男女不限':'Either',
@@ -47,5 +52,60 @@ yeeyi_dict={
 #TODO Start to handle the time and clean all the time up.
 def yeeyi_content_process(content):
     # Actually this is a dictionary function to convert chinese to english
-    not_find = "Unknow!" + content
+    not_find = "Unknown!" + content
     return yeeyi_dict.get(content,not_find)
+
+def cleanup_marks(page_content):
+    #best way to clean up all the hidden information.
+    span_count = 1
+    font_count = 1
+    for span in page_content.find_all('span', style='display:none'):
+        span.decompose()
+        span_count += 1
+    for font in page_content.find_all('font', style = "font-size:10px;color:#FFF"):
+        font.decompose()
+        font_count += 1
+    print("Finish all the cleaning up, I have find " + str(span_count) + " hidden span and " + str(font_count) + " hidden font")
+
+
+def information_pickup(page_content):
+    #this function is aiming at sort up all the information.
+    item={}
+    #clean up all the text
+    for p in page_content.find_all('p'):
+        if p.span:
+            item_title = str(yeeyi_title_process(p.span.text))
+            p.span.extract()
+            item_content = re.sub('\\xa0', '', p.text)
+            if item_title == "Property Rent":
+                item_content = yeeyi_content_process(item_content)
+            elif item_title == "Phone": #vaild phone number
+                phone_vaild = re.search("\d{9,11}", item_content)
+                if phone_vaild:
+                    item_content = phone_vaild.group(0)
+                else:
+                    item_content = "Unknown!" + item_content
+            else:
+                item_content = yeeyi_content_process(item_content)
+            item[item_title] = item_content
+    #find out all the images
+    imgs =[img['src'] for img in page_content.find_all('img')]
+    item['org_imgs']=imgs
+    if any(domain in s for s in imgs):
+        item['org_imgs'] = imgs
+    elif imgs:
+        for key, img in enumerate(imgs):
+            imgs[key] = 'http://' + domain + img
+            item['org_imgs'] = imgs
+    return item
+
+def get_publish_time(index):
+    #Get the publishing data
+    time_stamp_pre = index.find('div', class_="pti")
+    time_stamp_pre = re.search("\d{4}-\d{1,2}-\d{1,2} \d{1,2}:\d{1,2}",time_stamp_pre.em.text)
+    if time_stamp_pre:
+        time_stamp = time.strptime(time_stamp_pre.group(0),"%Y-%m-%d %H:%M")
+        time_pub = time.strftime("%d-%m-%Y %H:%M", time_stamp)
+    else:
+        time_pub = "Unknown!" + time_stamp_pre
+    return time_pub
--- reantal_yeeyi.py
+++ reantal_yeeyi.py
@@ -1,52 +1,20 @@
 __author__ = 'yuerzx'
 from bs4 import BeautifulSoup
-import re
 import requests
 import pre_process
-
-def cleanup_marks(page_content):
-    #best way to clean up all the hidden information.
-    span_count = 1
-    font_count = 1
-    for span in page_content.find_all('span', style='display:none'):
-        span.decompose()
-        span_count += 1
-    for font in page_content.find_all('font', style = "font-size:10px;color:#FFF"):
-        font.decompose()
-        font_count += 1
-    print("Finish all the cleaning up, I have find " + str(span_count) + " hidden span and " + str(font_count) + " hidden font")
-
-def information_pickup(page_content):
-    #this function is aiming at sort up all the information.
-    item={}
-    #clean up all the text
-    for p in page_content.find_all('p'):
-        if p.span:
-            item_title = str(pre_process.yeeyi_title_process(p.span.text))
-            p.span.extract()
-            item_content = re.sub('\\xa0', '', p.text)
-            if item_title != "Details":
-                item_content = pre_process.yeeyi_content_process(item_content)
-            item[item_title] = item_content
-    #find out all the images
-    imgs =[img['src'] for img in page_content.find_all('img')]
-    item['imgs']=imgs
-    if any(domain in s for s in imgs):
-        item['imgs'] = imgs
-    elif imgs:
-        for key, img in enumerate(imgs):
-            imgs[key] = 'http://' + domain + img
-            item['imgs'] = imgs
-    print(item)
+import re
+import time
 
 #hide the system as a chrome
 header = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.52 Safari/536.5"}
 html = requests.get("http://www.yeeyi.com/bbs/forum.php?mod=viewthread&tid=1481166", headers=header)
 index = BeautifulSoup(html.content,from_encoding='GBK')
+#Get the publishing data
+time_pub = pre_process.get_publish_time(index)
 
-domain = "yeeyi.com/bbs/"
-
-
+#Prepare all the documents content
 page_content = index.find('td', class_='t_f')
-cleanup_marks(page_content)
-information_pickup(page_content)
+pre_process.cleanup_marks(page_content)
+item = pre_process.information_pickup(page_content)
+item["publish_time"] = time_pub
+print(item)
