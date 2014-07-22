de3960accdb665d3ecdcefbd0b4a6d7f7f9df47b
Han Sun <HanSun@1230.me>
2014-07-22 05:45:42 -0700

    information system for ptv

---
+++ ptv/ptv.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+#same structure but different address. Easy!!:)
+html = requests.get("http://ptv.vic.gov.au/getting-around/stations-and-stops/metropolitan-buses/")
+dom = BeautifulSoup(html.content)
+header = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.52 Safari/536.5"}
+a_list = dom.find("div", id="alpha-list")
+f = open('buses.csv', 'w', newline='')
+writer = csv.writer(f, delimiter=',',quoting=csv.QUOTE_NONE, quotechar='')
+writer.writerow(['Suburb', 'link'])
+address_list = {}
+s_counter = 0
+e_counter = 0
+details_in_link = ""
+for x in a_list.findAll("li"):
+    details_link = x.a['href']
+    surburb = x.a.text
+    s_counter += 1
+    print("We are processing %s"%surburb)
+    writer.writerow([surburb, details_link])
+
+print("We got %d in total"%s_counter)
+
---
+++ ptv/surburb_detail.py
@@ -0,0 +1,51 @@
+__author__ = 'yuerzx'
+
+import requests
+from requests.adapters import HTTPAdapter
+from bs4 import BeautifulSoup
+import csv
+import time
+
+header = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.52 Safari/536.5"}
+filename = "buses" # trams or trains
+f = open(filename +'.csv', 'r', newline='')
+fw = open(filename+ '_step1.csv', 'w', newline ='')
+s_counter = 0 
+e_counter = 0
+reader = csv.reader(f, delimiter=',',quoting=csv.QUOTE_NONE, quotechar='')
+writer = csv.writer(fw, delimiter=',',quoting=csv.QUOTE_NONE, quotechar='')
+writer.writerow(['S_Name','suburb','link' ])
+next(reader)
+for row in reader:
+    suburb = row[0]
+    link = row[1]
+    html = None
+    try:
+        html = requests.get(link, headers=header, timeout = 3)
+    except:
+        try:
+            html = requests.get(link, headers=header, timeout = 3)
+        except:
+            writer.writerow(['Error', suburb, 'Error'])
+            e_counter += 1
+            pass
+        pass
+    if html is not None:
+        if html.status_code == 200:
+            s_counter += 1
+            detail_dom = BeautifulSoup(html.content)
+            stop_list = detail_dom.find('div', id="content")
+            print("We are processing %s"%suburb)
+            num = 0
+            for x in stop_list.findAll("li"):
+                num += 1
+                details_name = x.a.text
+                details_in_link = x.a['href']
+                writer.writerow([details_name, suburb, details_in_link])
+                print("The number %d"%num)
+    else:
+        e_counter += 1
+        writer.writerow(['Error', suburb, 'Error'])
+f.close()
+fw.close()
+print("We got %d in total with %d errors"%(s_counter,e_counter))
