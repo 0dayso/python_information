14baec654b2122c411d2a3c985fa6db7b206d822
Han Sun <HanSun@1230.me>
2014-07-22 05:47:06 -0700

    information system for ptv

--- ptv/ptv.py
+++ ptv/ptv.py
@@ -5,7 +5,7 @@ import csv
 html = requests.get("http://ptv.vic.gov.au/getting-around/stations-and-stops/metropolitan-buses/")
 dom = BeautifulSoup(html.content)
 header = {
-"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.52 Safari/536.5"}
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.52 Safari/536.5"}
 a_list = dom.find("div", id="alpha-list")
 f = open('buses.csv', 'w', newline='')
 writer = csv.writer(f, delimiter=',', quoting=csv.QUOTE_NONE, quotechar='')
